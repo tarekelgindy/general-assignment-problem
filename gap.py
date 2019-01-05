b6e5750d7eeaf448f5a1a52ee499810e36740aa3
Tarek Elgindy <tarek.elgindy@nrel.gov>
2019-01-04 19:02:49 -0700

    UNFINISHED - new almost addresses issues with augmenting path limits

--- gap.py
+++ gap.py
@@ -1,33 +1,36 @@
 import networkx as nx
 
-def adjust_graph(G,path,demand_nodes,assignments):
+def adjust_graph(G,path,demand_nodes,adjusted_supplies,assignments):
     all_edges = G.edges()
     edge_sizes = set()
-    all_weights = nx.get_edge_attributes(G,'weight')
     for i in range(len(path)):
-        e = path[i]
-        if not e in all_weights:
-            e = (e[1],e[0])
-        edge_sizes.add(all_weights[e])
+        edge_sizes.add(G.edges[path[i]]['weight'])
     min_edge = min(edge_sizes)
 
+    #import pdb;pdb.set_trace()
     cycle_addition = []
     cycle_subtraction = []
     for i in range(len(path)):
-        e = path[i]
-        if not e in all_weights:
-            e = (e[1],e[0])
-        cycle_addition.append(all_weights[e] + ((-1)**i)*min_edge)
-        cycle_subtraction.append(all_weights[e] + ((-1)**(i+1))*min_edge)
+        edge_weight = G.edges[path[i]]['weight']
+        cycle_addition.append(edge_weight + ((-1)**i)*min_edge)
+        cycle_subtraction.append(edge_weight + ((-1)**(i+1))*min_edge)
 
     nodes_to_remove = set()
     use_subtraction = None #Lock in which augmentation we're using once we find a node to remove
+    new_weights = []
     for i in range(len(path)):
         if cycle_subtraction[i] == 0 and (use_subtraction is None or use_subtraction):
-            use_subtraction = True
-            nodes_to_remove.add(path[i])
+            new_weights = cycle_subtraction
+            break
         if cycle_addition[i] == 0 and (use_subtraction is None or not use_subtraction):
+            new_weights = cycle_addition
+            break
             use_subtraction = False
+    for i in range(len(path)):
+        G.edges[path[i]]['weight'] = new_weights[i]
+        if new_weights[i] ==0:
+            nodes_to_remove.add(path[i])
+        if path[i][0] in demand_nodes and demand_nodes[path[i][0]] == new_weights[i] ==0 or path[i][1] in demand_nodes and demand_nodes[path[i][1]] == new_weights[i]:
             nodes_to_remove.add(path[i])
 
     cnt = 0
@@ -35,31 +38,31 @@ def adjust_graph(G,path,demand_nodes,assignments):
         if e[0] in demand_nodes:
             assignments[e[0]] = e[1]
             cnt+=1
-            removable_edges = G.edges(e[0])
+            removable_edges = list(G.edges(e[0]))
             for tr in removable_edges:
                 G.remove_edge(tr[0],tr[1])
         if e[1] in demand_nodes:
             assignments[e[1]] = e[0]
             cnt+=1
-            removable_edges = G.edges(e[1])
+            removable_edges = list(G.edges(e[1]))
             for tr in removable_edges:
                 G.remove_edge(tr[0],tr[1])
     return cnt
 
-def augmenting_path(G,demand_nodes,assignments):
+def augmenting_path(G,demand_nodes,adjusted_supplies, assignments):
     reduced_elements = 0
     while 1:
         try:
             cycle = nx.find_cycle(G) #Done arbitrarily. Maybe better to select one specifically?
-            reduced_elements+=adjust_graph(G,cycle,demand_nodes,assignments)
-            #print("Cycle: "+str(cycle))
+            reduced_elements+=adjust_graph(G,cycle,demand_nodes,adjusted_supplies, assignments)
+            print("Cycle: "+str(cycle))
         except nx.NetworkXNoCycle:
             break
     for i in G.nodes(): # Done arbitrarily. Maybe select one specifically?
         if G.degree(i) == 1:
             path = list(nx.dfs_edges(G,source=i))
-            #print("Path "+str(path))
-            reduced_elements+=adjust_graph(G,path,demand_nodes,assignments)
+            print("Path "+str(path))
+            reduced_elements+=adjust_graph(G,path,demand_nodes,adjusted_supplies, assignments)
             break
     return reduced_elements
 
@@ -94,8 +97,8 @@ else:
     G.add_node('supply_slack',demand = (total_demand+total_supply)- total_supply + total_demand)
 
 G.add_edge('demand_slack','supply_slack',weight = -1*max_supply,capacity=total_demand+total_supply) #might this cause numerical issues?
-print(total_supply,total_demand)
-print(nx.get_node_attributes(G,'demand'))
+#print(total_supply,total_demand)
+#print(nx.get_node_attributes(G,'demand'))
 for i in connection_list:
     for j in connection_list[i]:
         G.add_edge(i,j,weight=node_supply[j]*-1,capacity = node_demand[i]) #may help get more integer solutions?
@@ -144,6 +147,9 @@ for i in node_demand.keys():
 
 
 G2 = nx.Graph()
+adjusted_supplies = {}
+for i in node_supply:
+    adjusted_supplies[i] = node_supply[i]
 for i in node_demand:
     for j in flow_dict[i]:
         if j== 'supply_slack' and flow_dict[i][j] >0:
@@ -154,11 +160,12 @@ for i in node_demand:
             break
     if i not in assignments:
         for j in flow_dict[i]:
-            if j!='supply_slack':
+            if j!='supply_slack' and flow_dict[i][j]!=0:
                 G2.add_edge(i,j, weight=flow_dict[i][j]) #for simplicity keep the edges integer
+                adjusted_supplies[j] = adjusted_supplies[j]-flow_dict[i][j] #i.e. reduce the capacity of the node
 
 while 1:
-    reduced = augmenting_path(G2,node_demand,assignments)
+    reduced = augmenting_path(G2,node_demand,adjusted_supplies,assignments)
     if reduced == 0:
         break
 
